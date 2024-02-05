fa5cd7581088606c4b08201657550e6bf2544c32
evieteru <eviezushi@Nod>
2024-02-05 06:36:27 -0600

    Implemented updateStudentGradeById

--- src/HW2/StudentManager.java
+++ src/HW2/StudentManager.java
@@ -1,6 +1,25 @@
 package HW2;
 
 public class StudentManager {
+	
+	
+	
+public boolean updateStudentGradeById(int id, double grade) {
+		
+		if (searchStudentById(id) == false) {
+				return false;
+		}
+		
+		else {
+			for(Student currentStudent : students) {
+				if(currentStudent.getId() == id) {
+					currentStudent.setGrade(grade);
+					return true;
+				}
+			}
+			
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
