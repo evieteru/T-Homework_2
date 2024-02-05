5af1ae8dc70427228edcb7832419104392624afc
evieteru <eviezushi@Nod>
2024-02-05 06:20:58 -0600

    Implemented fields and getters and setters.

--- src/HW2/Student.java
+++ src/HW2/Student.java
@@ -1,5 +1,32 @@
 package HW2;
 
 public class Student {
+	
+	int id;
+	double grade;
+	String name;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getGrade() {
+		return grade;
+	}
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
