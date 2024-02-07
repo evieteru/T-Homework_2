044425f79cca5c3447695503b0ba9e8c7543d6c1
Hoa Quan <130907835+NhutQuan03@users.noreply.github.com>
2024-02-07 10:56:29 -0600

    Update StudentManager.java

--- src/HW2/StudentManager.java
+++ src/HW2/StudentManager.java
@@ -2,6 +2,31 @@ package HW2;
 
 public class StudentManager {
 
+	public void displayStudents() {
+    int totalStudents = 0;
+    for (Student student : students) {
+        if (student != null) {
+            totalStudents++;
+        }
+    }
+
+    if (totalStudents == 0) {
+        System.out.println("Error, check student ID, Null objects in array if something get removed and there are nothing in there");
+    } else {
+        System.out.println("Student List:");
+        System.out.printf("%-5s%-15s%-10s%n", "ID", "Name", "Grade");
+        System.out.println("###########################");
+        for (Student student : students) {
+            if (student != null) {
+                System.out.printf("%-5d%-15s%-10.2f%n", student.getId(), student.getName(), student.getGrade());
+            }
+        }
+        System.out.println("###########################");
+        System.out.println("Total students: " + totalStudents);
+    }
+}
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
