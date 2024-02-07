653f1c5cec2fcd65882659bb9c43b2998f3bac0a
Hoa Quan <130907835+NhutQuan03@users.noreply.github.com>
2024-02-07 10:50:03 -0600

    Update Student.java

--- src/HW2/Student.java
+++ src/HW2/Student.java
@@ -2,4 +2,28 @@ package HW2;
 
 public class Student {
 
+  private int id;
+  private String name;
+  private double grade;
+    
+  public Student() {
+  }
+
+  // Contructor
+  public Student(int id, String name, double grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+           "id=" + id +
+           ", name='" + name + '\'' +
+           ", grade=" + grade +
+           '}';
+    }
+
+
 }
